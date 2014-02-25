df8845e5dc3571b5dfc1d0d7afd068c09a635c9f
Donaging <donaging@live.com>
2014-02-25 19:27:02 +0100

    Creazione prima Alpha
    Non riesco a visualizzare il sito

---
+++ Vubico/Vubico/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Vubico.Resources;
+
+namespace Vubico
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Costruttore
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+ private void HomeClick(object sender, RoutedEventArgs e)
+        {
+            string site = "https://www.m.google.com";    
+            WebBrowser wb = new WebBrowser();
+            wb.Navigate(new Uri(site, UriKind.Absolute));
+            wb.IsScriptEnabled = true;
+        }
+    }
+}
