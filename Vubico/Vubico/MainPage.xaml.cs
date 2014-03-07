5555b188d129f9afb84764a7dd9e148d2fc85f80
Donaging <donaging@live.com>
2014-03-07 18:42:57 +0100

    ridimensione explorer

--- Vubico/Vubico/MainPage.xaml.cs
+++ Vubico/Vubico/MainPage.xaml.cs
@@ -17,12 +17,28 @@ namespace sdkMiniBrowserCS
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void button1_Click(object sender, RoutedEventArgs e)
+     /*   private void button1_Click(object sender, RoutedEventArgs e)
         {
             string site;
             site = textBox1.Text;
             webBrowser1.Navigate(new Uri(site, UriKind.Absolute));
+           
+        }*/
+
+        private void WebBrowser_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            webBrowser1.Navigate(new Uri("https://vubi.co/"));
+            webBrowser1.Navigating += OnNavigating;
         }
 
+        private void OnNavigating(object sender, NavigatingEventArgs e)
+        {
+            //every navigation to any sites that is not https://vubi.co will be block
+            if (e.Uri.OriginalString != "https://vubi.co")
+            {
+                e.Cancel = true;
+            }
+        }
+      
     }
 }
